bd3e1f05302e1273b24c46bf2f1c756cfa19f73b
Rijo John <rijo.john@infracloud.io>
2025-01-17 08:36:20 +0530

    added duplicateline remover amended file perm checker to use functions

---
+++ basic/duplicate_line_remover.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file=$1
+
+if [ ! -f $file ]; then
+  echo "Error: File not found"
+  exit 1
+fi
+
+if [ ! -s $file ]; then
+  echo "File is empty"
+  exit 1
+fi
+
+sort $file | uniq >uniqueFile
--- basic/file_permission_checker.sh
+++ basic/file_permission_checker.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+parse_perm() {
+  perm=$1
+  perm_read="No"
+  perm_write="No"
+  perm_x="No"
+  if [[ $perm == *r* ]]; then
+    perm_read="Yes"
+  fi
+  if [[ $perm == *w* ]]; then
+    perm_write="Yes"
+  fi
+  if [[ $perm == *x* ]]; then
+    perm_x="Yes"
+  fi
+  echo "Read-$perm_read, Write-$perm_write, Execute-$perm_x"
+}
+
 file=$1
 
 if [ ! -f $file ]; then
@@ -10,48 +27,11 @@ permissions=$(ls -l $file | awk '{print $1}')
 
 #owner
 owner=$(echo $permissions | awk '{print substr($1, 2, 3)}')
-owner_read="No"
-owner_write="No"
-owner_x="No"
-if [[ $owner == *r* ]]; then
-  owner_read="Yes"
-fi
-if [[ $owner == *w* ]]; then
-  owner_write="Yes"
-fi
-if [[ $owner == *x* ]]; then
-  owner_x="Yes"
-fi
-echo "Owner: Read-$owner_read, Write-$owner_write, Execute-$owner_x"
+echo "Owner: $(parse_perm $owner)"
 
 #group
 group=$(echo $permissions | awk '{print substr($1, 5, 3)}')
-group_read="No"
-group_write="No"
-group_x="No"
-if [[ $group == *r* ]]; then
-  group_read="Yes"
-fi
-if [[ $group == *w* ]]; then
-  group_write="Yes"
-fi
-if [[ $group == *x* ]]; then
-  group_x="Yes"
-fi
-echo "Group: Read-$group_read, Write-$group_write, Execute-$group_x"
-
+echo "Group: $(parse_perm $group)"
 #others
 others=$(echo $permissions | awk '{print substr($1, 8, 3)}')
-others_read="No"
-others_write="No"
-others_x="No"
-if [[ $others == *r* ]]; then
-  others_read="Yes"
-fi
-if [[ $others == *w* ]]; then
-  others_write="Yes"
-fi
-if [[ $others == *x* ]]; then
-  others_x="Yes"
-fi
-echo "Others: Read-$others_read, Write-$others_write, Execute-$others_x"
+echo "Others: $(parse_perm $others)"
